3152c8106fcedfb80430e2a37d668f0acc6dbdb4
226407 <226407@shakopeeschools.org>
2020-02-18 23:17:02 -0600

    Adding and edititing Javascript examples

--- Version5.0/user32/JS/SampleJS.js
+++ Version5.0/user32/JS/SampleJS.js
@@ -1,11 +1,11 @@
 function changeText() {
     document.getElementById("textChange").innerHTML = "<div class = 'dwa'> Thanks for liking my Webpage</div>";
 }
+
 function appearText() {
     document.getElementById("textAppear").innerHTML = "<div class = 'appear'> This text appeared</div>";
 }
 
-
-
-
-
+function txtSelect() {
+    document.getElementById("txtselect").innerHTML = "Quality Job";
+}
