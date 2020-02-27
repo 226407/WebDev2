27f0561d449d2022d01e3a116941b54c66fb8277
226407 <226407@shakopeeschools.org>
2020-02-27 14:23:39 -0600

    Edits

--- Version7.0/user32/JS/SampleJS.js
+++ Version7.0/user32/JS/SampleJS.js
@@ -1,2 +1,38 @@
-function changeTxt() {
-    document.getElementById("txtChange").innerHTML = "<div class = 'test'> The test worked</div>";
+body {
+    width: 500 px;
+
+    margin: auto;
+    text - align: center;
+}
+
+dl {
+
+}
+
+dd {
+    margin: 0;
+    padding: 1 em 0;
+}
+
+dt {
+    cursor: pointer;
+    font - weight: bold;
+    font - size: 1.5 em;
+    line - height: 2 em;
+    background: #e3e3e3;
+    border - bottom: 1 px solid #c5c5c5;
+    border - top: 1 px solid white;
+}
+
+dt: first - child {
+    border - top: none;
+}
+
+dt: nth - last - child(2) {
+        border - bottom: none;
+    }
+    .hide {
+        display: none;
+    }
+
+(Curently doesnt work)
